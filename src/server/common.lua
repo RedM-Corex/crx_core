077cb46939e5995b0220632139faa71181b03b0a
TijmenK <tijmenk2006@gmail.com>
2024-06-13 09:49:35 +0200

    ignore items using ox inventory

--- src/server/common.lua
+++ src/server/common.lua
@@ -17,11 +17,11 @@ MySQL.ready(function()
     Core.DatabaseConnected = true
     CRX.Debug.Info("Database connected")
     
-    local items = MySQL.Sync.fetchAll("SELECT * FROM `players`")
-    for _, v in ipairs(items) do
-        CRX.Items[v.name] = {label = v.label, weight = v.weight, canRemove = v.canRemove}
-    end
+    -- local items = MySQL.Sync.fetchAll("SELECT * FROM `players`")
+    -- for _, v in ipairs(items) do
+    --     CRX.Items[v.name] = {label = v.label, weight = v.weight, canRemove = v.canRemove}
+    -- end
 
-    CRX.Debug.Info(("Loaded %s items."):format(#items))
+    -- CRX.Debug.Info(("Loaded %s items."):format(#items))
 
 end)
